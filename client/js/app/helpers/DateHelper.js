66ce1c6872fe4ce5ab988c24f894ca936642bd77
Caue Queiroz <cauenqueiroz@gmail.com>
2017-07-30 04:40:06 -0300

    Finish module 04

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -1,13 +1,17 @@
 class DateHelper {
+
     constructor() {
+
         throw new Error("Essa classe nao pode ser instanciada.");
     }
     
     static dataParaTexto(data) {
+
         return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
     }
 
     static textoParaData(texto) {
+        
         if ( !/\d{4}-\d{2}-\d{2}/.test(texto) )
             throw new Error("Data precisa estar no formato aaaa-mm-dd.");
 
--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,5 +1,7 @@
 class Negociacao {
+
     constructor(data, quantidade, valor) {
+
         this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
@@ -8,18 +10,22 @@ class Negociacao {
     }
 
     get data() {
+
         return new Date(this._data.getTime());
     }
 
     get quantidade() {
+
         return this._quantidade
     }
     
     get valor() {
+
         return this._valor;
     }
 
     get volume() {
+        
         return this._quantidade * this._valor;
     }
 }
