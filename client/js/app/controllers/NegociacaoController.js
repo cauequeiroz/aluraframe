987bb28a63177d9c6e4846a153cd140a2dac09e0
Caue Queiroz <cauenqueiroz@gmail.com>
2017-09-07 16:34:54 -0300

    Finish module 05

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -9,14 +9,17 @@ class NegociacaoController {
         this._inputValor = $('#valor');
 
         this._listaNegociacoes = new ListaNegociacoes();
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        this._negociacoesView.update(this._listaNegociacoes);
     }
 
     adiciona(event) {
 
         event.preventDefault();
-        
-        this._listaNegociacoes.adiciona( this._criaNegociacao() );
-        this._limpaFormulario();        
+
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._listaNegociacoes);
+        this._limpaFormulario();
     }
 
     _criaNegociacao() {
