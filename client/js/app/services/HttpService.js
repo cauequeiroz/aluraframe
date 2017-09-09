8baf5d0d54fdb4c5187401bb5372b75b7c9ef1d7
Caue Queiroz <cauenqueiroz@gmail.com>
2017-09-08 22:55:25 -0300

    Finish course

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -21,4 +21,27 @@ class HttpService {
             xhr.send();
         });
     }
+
+    post(url, dado) {
+
+        return new Promise((resolve, reject) => {
+
+            let xhr = new XMLHttpRequest();
+
+            xhr.open('GET', url);
+            xhr.setRequestHeader('Content-type', 'application/json');
+
+            xhr.onreadystatechange = () => {
+
+                if ( xhr.readyState == 4 ) {
+
+                    xhr.status == 200
+                        ? resolve(JSON.parse(xhr.responseText)) 
+                        : reject(xhr.responseText);
+                }
+            }
+
+            xhr.send(JSON.stringify(dado));
+        });
+    }
 }
