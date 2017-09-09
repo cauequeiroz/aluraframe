e2792589448824ab61fb666aa9a8c6f80efe94d3
Caue Queiroz <cauenqueiroz@gmail.com>
2017-09-09 20:04:20 -0300

    Finish module 05

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -2,46 +2,24 @@ class HttpService {
 
     get(url) {
 
-        return new Promise((resolve, reject) => {
-
-            let xhr = new XMLHttpRequest();
-
-            xhr.open('GET', url);
-
-            xhr.onreadystatechange = () => {
-
-                if ( xhr.readyState == 4 ) {
-
-                    xhr.status == 200
-                        ? resolve(JSON.parse(xhr.responseText)) 
-                        : reject(xhr.responseText);
-                }
-            }
-
-            xhr.send();
-        });
+        return fetch(url)
+            .then(res => this._handleErrors(res))
+            .then(res => res.json());
     }
 
     post(url, dado) {
 
-        return new Promise((resolve, reject) => {
+        return fetch(url, {
+            headers: { 'Content-type': 'application/json' },
+            method: 'post',
+            body: JSON.stringify(dado)
+        })
+        .then(res => this._handleErrors(res));
+    }
 
-            let xhr = new XMLHttpRequest();
-
-            xhr.open('GET', url);
-            xhr.setRequestHeader('Content-type', 'application/json');
-
-            xhr.onreadystatechange = () => {
-
-                if ( xhr.readyState == 4 ) {
-
-                    xhr.status == 200
-                        ? resolve(JSON.parse(xhr.responseText)) 
-                        : reject(xhr.responseText);
-                }
-            }
-
-            xhr.send(JSON.stringify(dado));
-        });
+    _handleErrors(res) {
+        
+        if ( !res.ok ) throw new Error(res.statusText);
+        return res;
     }
 }
