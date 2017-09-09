7c9d53cae08d49e93f38043a12fba225f0171fe3
Caue Queiroz <cauenqueiroz@gmail.com>
2017-09-09 07:36:42 -0300

    Hide message after few seconds

--- client/js/app/views/MensagemView.js
+++ client/js/app/views/MensagemView.js
@@ -1,7 +1,27 @@
 class MensagemView extends View {
 
+    constructor(elemento) {
+        
+        super(elemento);
+        this._timer = null;
+    }
+
     template(model) {
 
         return model.texto ? `<p class="alert alert-info">${model.texto}</p>` : '<p></p>';
+    }
+
+    update(model) {
+        
+        this._elemento.innerHTML = this.template(model);
+        
+        if ( model.texto ) {
+            if ( this._timer ) clearInterval(this._timer);
+
+            this._timer = setTimeout(() => {
+                this._elemento.innerHTML = '<p></p>';
+                this._timer = null;
+            }, 2000);
+        }
     }
 }
